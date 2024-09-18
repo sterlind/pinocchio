3277f0e2107c5583173c06c38ef26e3874991f67
Star Dorminey <sterlind@microsoft.com>
2024-09-17 22:47:56 -0700

    Meh

--- ppu.sv
+++ ppu.sv
@@ -1,7 +1,16 @@
+`include "ram.sv"
+
 typedef enum logic [3:0] {
     LCDC = 4'h0,
     LY = 4'h4
 } ppu_reg_t;
+
+typedef enum logic [1:0] {
+    PHASE_HBLANK,
+    PHASE_VBLANK,
+    PHASE_OAM_SCAN,
+    PHASE_DRAW
+} ppu_phase_t;
 
 typedef struct packed {
     bit ena;
@@ -16,25 +25,67 @@ typedef struct packed {
 
 module ppu_m (
     input wire clk,
+    // DMA:
+    // When DMA is active, PPU will request data in (and take over the CPU's bus.)
+    output wire [15:0] dma_src_addr,
+    input wire [7:0] dma_d_in,
+    input wire dma_active,
     // Regs:
     input wire [3:0] reg_addr,
     input wire [7:0] reg_d_wr,
     output logic [7:0] reg_d_rd,
     input wire reg_write,
     // VRAM:
-    input wire [12:0] vram_addr,
+    input wire [12:0] vram_addr_in,
     input wire [7:0] vram_d_wr,
     output logic [7:0] vram_d_rd,
-    input wire vram_write,
+    input wire vram_write_in,
     // OAM:
-    input wire [7:0] oam_addr,
+    input wire [7:0] oam_addr_in,
     input wire [7:0] oam_d_wr,
     output logic [7:0] oam_d_rd,
-    input wire oam_write,
+    input wire oam_write_in,
     // Display:
     output wire lcd_clk, lcd_ena,
     output wire lcd_hsync, lcd_vsync,
     output wire [1:0] lcd_color
+);
+    ppu_phase_t phase;
+
+    reg [12:0] vram_addr;
+    reg vram_write;
+    ram #(WORDS = 8192) vram (
+        .clk(clk),
+        .addr(vram_addr),
+        .write(vram_write),
+        .d_in(vram_d_wr),
+        .d_out(vram_d_rd)
+    );
+    always_comb case (phase)
+        PHASE_DRAW: begin vram_addr = renderer.vram_addr; vram_write = 0; end
+        default: begin vram_addr = vram_addr_in; vram_write = vram_write_in; end
+    endcase
+
+    reg [6:0] oam_addr;
+    reg [5:0] oam_dma_src_base;
+    ram #(WORDS = 80, WIDTH = 16) oam (
+        .clk(clk),
+        .addr(oam_addr),
+        .write(oam_write)
+    );
+    always_comb 
+
+    ppu_renderer renderer (
+        .clk(clk),
+        .vram_in(vram.d_out)
+    );
+endmodule
+
+module ppu_renderer(
+    input wire clk,
+    output wire [12:0] vram_addr,
+    input wire [7:0] vram_in,
+    input wire [5:0]
 );
 endmodule
 
---
+++ ram.sv
@@ -0,0 +1,16 @@
+module ram #(parameter WORDS = 512, parameter WIDTH = 8) (
+    input wire clk,
+    input wire [D-1:0] addr,
+    input wire write,
+    input wire [W:0] d_in,
+    output logic [W:0] d_out
+);
+    localparam D = $clog2(WORDS);
+    localparam W = WIDTH-1;
+
+    reg [W:0] mem[0:WORDS-1];
+    always_ff @(posedge clk) begin
+        d_out <= mem[addr];
+        if (write) mem[addr] <= d_in;
+    end
+endmodule
