e5831904852890a8461d37651aee5c0474606fb6
sterlind <sterl@AHHTJEE>
2024-09-25 17:53:24 -0700

    DMA change somehow messed the Nintendo logo up?

--- rtl/dmg_main.sv
+++ rtl/dmg_main.sv
@@ -4,6 +4,7 @@ typedef enum logic [15:0] {
     WRAM        = 16'b110x_xxxx_xxxx_xxxx,
     OAM         = 16'hfexx,
     HIDEROM     = 16'hff50,
+    OAM_DMA     = 16'hff46,
     PPU_REG     = 16'hff4x
 } mem_mask;
 
@@ -36,8 +37,11 @@ module dmg_main(
     assign irq = {7'b0, irq_vblank};
 
     reg [7:0] bus_in, cpu_d_out;
-    wire [15:0] cpu_addr /* synthesis syn_keep=1 */;
-    wire cpu_write;
+    wire [15:0] cpu_addr, bus_addr , dma_src_addr;
+    reg bus_write, cpu_write;
+    reg dma_active;
+    assign bus_addr = dma_active ? dma_src_addr : cpu_addr;
+    assign bus_write = dma_active ? 0 : cpu_write;
     sm83 cpu (
         .clk(clk),
         .ce(cpu_ce),
@@ -54,13 +58,13 @@ module dmg_main(
     ppu_m ppu (
         .clk(clk),
         .d_wr(cpu_d_out),
-        .reg_addr(cpu_addr[3:0]),
+        .reg_addr(bus_addr[3:0]),
         .reg_d_rd(reg_d_rd),
         .reg_write(ppu_reg_write),
-        .oam_addr_in(cpu_addr[7:0]),
+        .oam_addr_in(bus_addr[7:0]),
         .oam_d_rd(oam_d_rd),
         .oam_write(oam_write),
-        .vram_addr_in(cpu_addr[12:0]),
+        .vram_addr_in(bus_addr[12:0]),
         .vram_d_rd(vram_d_rd),
         .vram_write_in(vram_write),
         .lcd_hsync(lcd_hsync),
@@ -79,7 +83,7 @@ module dmg_main(
         .ce(cpu_ce),
         .reset(~rst),
         .wre(wram_write),
-        .ad(cpu_addr[12:0]),
+        .ad(bus_addr[12:0]),
         .din(cpu_d_out)
     );
 
@@ -94,21 +98,34 @@ module dmg_main(
         .dout(boot_data)
     );
 
-    always_ff @(negedge clk)
-        if (~rst) hide_boot <= 0;
-        else if (cpu_addr == HIDEROM && cpu_write && |cpu_d_out) hide_boot <= 1'b1;
+    reg [7:0] dma_base;
+    reg [7:0] dma_idx;
+    assign dma_active = dma_idx != 8'h9f;
+    assign dma_src_addr = {dma_base, dma_idx};
 
-    assign rom_addr = cpu_addr[14:0];
+    always_ff @(negedge clk)
+        if (~rst) begin hide_boot <= 0; dma_base <= 8'hff; end
+        else begin
+            if (bus_write) case (bus_addr)
+                HIDEROM: hide_boot <= hide_boot | (|cpu_d_out);
+                OAM_DMA: begin dma_base <= cpu_d_out; dma_idx <= 8'b0; end
+            endcase
+            if (dma_active) dma_idx <= dma_idx + 1'b1;
+        end
+
+    assign rom_addr = bus_addr[14:0];
     always_comb begin
         ppu_reg_write = 0; vram_write = 0; ppu_reg_write = 0; wram_write = 0; oam_write = 0;
         bus_in = 8'hff;
-        casex (cpu_addr)
+        casex (bus_addr)
             16'hff00: bus_in = 8'h0f;
-            VRAM: begin bus_in = vram_d_rd; vram_write = cpu_write; end
-            WRAM: begin bus_in = wram_d_rd; wram_write = cpu_write; end
-            OAM: begin bus_in = oam_d_rd; oam_write = cpu_write; end
+            VRAM: begin bus_in = vram_d_rd; vram_write = bus_write; end
+            WRAM: begin bus_in = wram_d_rd; wram_write = bus_write; end
+            OAM: begin bus_in = oam_d_rd; oam_write = bus_write; end
             ADDR_ROM: begin bus_in = (hide_boot || |rom_addr[14:8]) ? rom_data : boot_data; end
-            PPU_REG: begin bus_in = reg_d_rd; ppu_reg_write = cpu_write; end
+            OAM_DMA: bus_in = dma_base;
+            PPU_REG: begin bus_in = reg_d_rd; ppu_reg_write = bus_write; end
         endcase
+        if (dma_active) oam_write = 1;
     end
 endmodule
