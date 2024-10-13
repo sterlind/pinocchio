d3da5ab2823e506389427bfdd300746d4e81265c
Star Dorminey <sterlind@microsoft.com>
2024-10-12 17:21:56 -0700

    Trying to repair OAM

--- sim/top_tb.sv
+++ sim/top_tb.sv
@@ -14,7 +14,7 @@ module top_tb;
 
     wire [14:0] rom_addr;
     wire [7:0] rom_data;
-    cart_prom cart (
+    cart_test_prom cart (
         .clk(tclk),
         .oce('x),
         .ce(1'b1),
@@ -30,9 +30,19 @@ module top_tb;
         .rom_data(rom_data)
     );
 
+    reg [15:0] r_af, r_bc, r_de, r_hl, r_sp, r_pc, r_zw;
+    assign r_af = {dmg.cpu.rf[A], dmg.cpu.flags};
+    assign r_bc = {dmg.cpu.rf[B], dmg.cpu.rf[C]};
+    assign r_de = {dmg.cpu.rf[D], dmg.cpu.rf[E]};
+    assign r_hl = {dmg.cpu.rf[H], dmg.cpu.rf[L]};
+    assign r_sp = {dmg.cpu.rf[SPH], dmg.cpu.rf[SPL]};
+    assign r_pc = {dmg.cpu.rf[PCH], dmg.cpu.rf[PCL]};
+    assign r_zw = {dmg.cpu.rf[Z], dmg.cpu.rf[W]};
+
     initial begin
         $dumpvars(0, dmg);
-        #(1024 * 1024 * 4);
+        $dumpvars(0, r_af, r_bc, r_de, r_hl, r_sp, r_pc, r_zw);
+        #(600 * 1024 * 4);
         $display("Done");
         $finish;
     end
